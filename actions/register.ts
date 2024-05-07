2b3a69c3b4f7d5d58f74ead9cfa33bb38ab35acb
NhicFlores <nflores@mankowindows.com>
2024-05-07 16:58:39 -0500

    installed bcrypt - added create user logic in register action

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,43 @@
 import { z } from "zod";
 import { RegisterSchema } from "@/schema";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 
-export async function register(formData:z.infer<typeof RegisterSchema>) {
-    const validatedFields = RegisterSchema.safeParse(formData);
+export async function register(formFields:z.infer<typeof RegisterSchema>) {
+    const validatedFields = RegisterSchema.safeParse(formFields);
 
     if(!validatedFields.success) {
         return { error: "invalid fields"};
     }
 
-    return {success: "registraion email sent"}
+    const { email, name, password, confirm_password } = validatedFields.data
+
+    if(password !== confirm_password) {
+        return { error: "passwords do not match! "};
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        }
+    });
+
+    if (existingUser) {
+        return { error: "Email already in use!" };
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    });
+
+    // NOTE TODO: send verification token email 
+
+    return {success: "User created!"};
     
 }
